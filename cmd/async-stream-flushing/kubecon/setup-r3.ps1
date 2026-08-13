a44941e26fdc1fd5eff20db47c652455881062fe
Bruno Baloi <brunobaloi@Brunos-MacBook-Pro.local>
2026-08-13 18:34:49 -0400

    Remove cmd folder

--- cmd/async-stream-flushing/kubecon/setup-r3.ps1
+++
@@ -1,4 +0,0 @@
-nats str add r3 `
-    --replicas=3 `
-    "--subjects=r3.*" `
-    --defaults
--- cmd/async-stream-flushing/kubecon/setup-r3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-nats str add r3 \
-    --replicas=3 \
-    --subjects='r3.*' \
-    --defaults
